2751cc69fbe27df3eabe78f41195d4647f0eda63
kevin444528 <100454428@alumnos.uc3m.es>
2023-10-26 12:19:34 +0200

    comentarios y limpieza

--- votacion-1.2.py
+++ votacion-1.2.py
@@ -10,8 +10,10 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class AplicacionRegistro:
     def __init__(self, ventana):
+        #Nombre de la base de datos
         self.nombre_basedatos='basedatos.csv'
         self.base_panda=pd.read_csv(self.nombre_basedatos)
+        #Interfaz de la venta de inicio sesion
         self.ventana = ventana
         self.ventana.title("Aplicación de Inicio de Sesión")
         self.dni_label = tk.Label(ventana, text="DNI:")
@@ -19,8 +21,6 @@ class AplicacionRegistro:
         self.contrasena_label = tk.Label(ventana, text="Contraseña:")
         self.contrasena_entry = tk.Entry(ventana, show="*")
         self.mensaje_error = tk.Label(ventana, text="", fg="red")
-
-        self.clave=None
 
         self.iniciar_sesion_button = tk.Button(ventana, text="Iniciar Sesión", command=self.iniciar_sesion)
         self.registrarse_button = tk.Button(ventana, text="Registrarse", command=self.abrir_ventana_registro)
@@ -33,29 +33,37 @@ class AplicacionRegistro:
         self.iniciar_sesion_button.grid(row=2, column=0, columnspan=2)
         self.registrarse_button.grid(row=3, column=0, columnspan=2)
         self.mensaje_error.grid(rows=5, column=0, columnspan=2)
+        #variable temporal que guarda el valor de la contraseña del usuario
+        self.clave = None
 
     def iniciar_sesion(self):
+        """Comprobacion del DNI y la contraseña que se introduce"""
+        #obtenemos el DNI
         dni = self.dni_entry.get()
         contrasena = self.contrasena_entry.get()
-        # Agrega la lógica de inicio de sesión aquí.
         usuario = self.buscar_usuario(self.nombre_basedatos,dni)
+        #Comprueba que el usuario esta en la base de datos
         if usuario is not None:
             con_by = bytes.fromhex(usuario[1])
             salt_by= bytes.fromhex(usuario[2])
+            #Comprueba la contraseña con el hash que tenemos guardado
             validez = self.verificar_clave(contrasena, con_by, salt_by)
             if validez:
                 self.mostrar_ventana_votacion()
                 self.clave=contrasena
-                print("entra")
+                self.mostrar_error("")
             else:
                 self.mostrar_error("Contraseña incorrecta")
         else:
             self.mostrar_error("Usuario no válido")
 
     def mostrar_error(self, mensaje):
+        """Adapata el mensaje de error de inicio de sesion"""
         self.mensaje_error.config(text=mensaje)
-
+    def mostrar_error_registro(self,mensaje):
+        pass
     def comprobar_fecha(self):
+        """Comprobacion del formato fecha"""
         fecha = self.fecha_nacimiento_entry.get()
         if len(fecha) != 10 or fecha[2] != "/" or fecha[5] != "/":
             self.fecha_mensaje_error.config(text="La fecha de nacimiento debe tener el formato: XX/XX/XXXX")
@@ -65,18 +73,23 @@ class AplicacionRegistro:
             return True
 
     def comprobar_dni(self):
+        """Comprobacion del formato DNI"""
         dni = self.dni_registro_entry.get()
         if len(dni) != 9 or dni[-1].isnumeric() or dni[-1] not in "TRWAGMYFPDXBNJZSQVHLCKE":
             self.dni_mensaje_error.config(text="DNI no válido: debe contener 8 dígitos y 1 letra")
             return False
         usuario = self.buscar_usuario(self.nombre_basedatos,dni)
+        #Comprobamos que no exista un usuario con el mismo DNI
         if usuario is not None:
             self.dni_mensaje_error.config(text="Ya existe un usuario registrado con este DNI")
             return False
+        self.dni_mensaje_error.config(text="")
         return True
 
     def comprobar_contrasena(self):
+        """Comprobacion del formato de contraseña"""
         contr = self.contrasena_registro_entry.get()
+        #Obliga que la contraseña tenga un minimo de longitud
         if len(contr) < 8:
             self.contrasena_mensaje_error.config(text="La contraseña debe tener al menos 8 caracteres")
             return False
@@ -85,10 +98,11 @@ class AplicacionRegistro:
             return True
 
     def mostrar_ventana_votacion(self):
+        """Interfaz de la ventana de votacion. Que se muestra despues de iniciar sesion"""
         self.ventana_votacion = tk.Toplevel()
         self.ventana_votacion.title("Votación")
         verdatos = tk.Button(self.ventana_votacion, text="Mis datos", command=self.ver_datos)
-        # Agrega botones para votar en esta ventana
+        # Llaman a la funcion votar pasandole como parametro la eleccion
         boton_opcion1 = tk.Button(self.ventana_votacion, text="Opción 1", command=lambda: self.votar(1))
         boton_opcion2 = tk.Button(self.ventana_votacion, text="Opción 2", command=lambda:self.votar(2))
         boton_opcion3 = tk.Button(self.ventana_votacion, text="Opción 3", command=lambda: self.votar(3))
@@ -102,31 +116,34 @@ class AplicacionRegistro:
         boton_opcion4.grid(row=6, column=1, columnspan=2)
 
     def votar(self,opcion):
+        """Añade o actuliza el voto a la lista del usuario"""
         self.base_panda=pd.read_csv("basedatos.csv")
         voto= "Opcion: "+ str(opcion)
+        #Buscamos el usuario en la base de datos
         usuario= self.base_panda.loc[self.base_panda["dni"]==self.dni_entry.get()]
         salt= bytes.fromhex(usuario["salt"].iloc[0])
+        #Encripatmos el voto
         voto_cif, nonce_voto = self.encriptar(voto,salt)
         voto_cif_str = voto_cif.hex()
         nonce_voto_str = nonce_voto.hex()
-        # Realizar las asignaciones
-        print(usuario["dni"],voto_cif, nonce_voto)
+        #Guardamos el voto cifrado con su nonce
         self.base_panda.loc[self.base_panda["dni"] == self.dni_entry.get(), "voto"] = voto_cif_str
         self.base_panda.loc[self.base_panda["dni"] == self.dni_entry.get(), "nonce_voto"] = nonce_voto_str
+        #Añadimos las modificaciones al fichero
         self.base_panda.to_csv("basedatos.csv", index=False)
-        #men= self.desencriptar(bytes.fromhex(usuario["nombre_cfr"].iloc[0]), bytes.fromhex(usuario["nonce_nombre"].iloc[0]),bytes.fromhex(usuario["salt"].iloc[0]))
-        #print(men)
 
     def ver_datos(self):
+        """Interfaz de la ventaa datos"""
         self.ventana_datos = tk.Toplevel(self.ventana)
         self.ventana_datos.title("Mis datos")
         titulo_label = tk.Label(self.ventana_datos, text="Datos", font=("bold", 14))
         titulo_label.pack()
+        #Buscamos el usuario
         usuario = self.buscar_usuario(self.nombre_basedatos,self.dni_entry.get())
         nombre=self.desencriptar(bytes.fromhex(usuario[3]), bytes.fromhex(usuario[4]),bytes.fromhex(usuario[2]))
-        #print(bytes.fromhex(usuario[3]), bytes.fromhex(usuario[4]),bytes.fromhex(usuario[2]),usuario[3],usuario[4],[2])
         apellido = self.desencriptar(bytes.fromhex(usuario[5]), bytes.fromhex(usuario[6]), bytes.fromhex(usuario[2]))
         fecha = self.desencriptar(bytes.fromhex(usuario[7]), bytes.fromhex(usuario[8]), bytes.fromhex(usuario[2]))
+        #Comprueba si el usuario ha votado
         if usuario[9] =="":
             voto = "aun no ha votado"
         else:
@@ -145,12 +162,14 @@ class AplicacionRegistro:
 
 
     def buscar_usuario(self, fichero,dni):
+        """Busca un usuario por su DNI en un csv"""
         with open(fichero, mode='r') as archivo_csv:
             lector_csv = csv.reader(archivo_csv)
             for fila in lector_csv:
                 if fila and fila[0] == dni:
                     return fila
     def addto_csv(self,fichero,nuevo_usuario):
+        """Añade una fila/usuario a un fichero csv """
         with open(fichero, mode='a', newline='') as archivo_csv:
             # Crea un objeto escritor de CSV
             escritor_csv = csv.writer(archivo_csv)
@@ -160,6 +179,9 @@ class AplicacionRegistro:
 
 
     def verificar_clave(self, intento, key, salt):
+        """Verifica una clave cifrada con Scrypt. Intento es la clave con la que se esta intentando entra
+        entrar a la cuenta de usuario y key es la contraseña cifrada guardada"""
+
         algoritmo = "Scrypt"
         key_length = 32
         kdf = Scrypt(
@@ -169,7 +191,9 @@ class AplicacionRegistro:
             r=8,
             p=1,
         )
+        #Usamos un try para que el mensaje de error en caso de no coincidir pare el programa
         try:
+            #Comprobamos si la contraseña es correcta
             kdf.verify(intento.encode('utf-8'), key)
             print(f"Información sobre el cifrado: \nEl algoritmo usado para cifrar es {algoritmo} y la longitud de la clave es {key_length}")
         except cryptography.exceptions.InvalidKey as e:
@@ -178,6 +202,7 @@ class AplicacionRegistro:
             return True
 
     def cifrar_clave(self, clave):
+        """Cifra una cadena de texto usando el algoritmo Scrypt"""
         algoritmo = "Scrypt"
         salt = os.urandom(16)
         key_length = 32
@@ -188,12 +213,13 @@ class AplicacionRegistro:
             r=8,
             p=1,
         )
+        #Transforma la cadena de texto en cadena de bit
         clave_bytes = clave.encode('utf-8')
         key = kdf.derive(clave_bytes)
         print(f"Información sobre el cifrado: \nEl algoritmo usado para cifrar es {algoritmo} y la longitud de la clave es {key_length}")
         return key, salt
     def derivar_clave(self,salt):
-        # derive
+        """Deriva la clave actual"""
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
@@ -205,10 +231,12 @@ class AplicacionRegistro:
         key = kdf.derive(clave_bytes)
         return key
     def encriptar(self, data,salt):
+        """Encripta datos con un salt paasado como parametro"""
         algoritmo = "AES"
+        #Transformamos los datos en cadena de bits
         data_b = data.encode('utf-8')
+        #Deriva la la llave que sera la contraseña del usuario actual
         key= self.derivar_clave(salt)
-        print("contraseña encrip: ",key)
 
         key_length = len(key)
         aesgcm = AESGCM(key)
@@ -220,6 +248,7 @@ class AplicacionRegistro:
         return ct, nonce
 
     def desencriptar(self, ct, nonce,salt):
+        """Desencripta datos con un determinado nonce y salt"""
         algoritmo = "AES"
         key = self.derivar_clave(salt)
         key_length = len(key)
@@ -229,6 +258,7 @@ class AplicacionRegistro:
         return dato.decode()
 
     def abrir_ventana_registro(self):
+        """Interfaz de la ventana de registro de usuario"""
         self.ventana_registro = tk.Toplevel(self.ventana)
         self.ventana_registro.title("Registro")
 
@@ -267,26 +297,30 @@ class AplicacionRegistro:
         self.contrasena_mensaje_error.grid(row=7, column=0, columnspan=2)
 
     def registrar_usuario(self):
+        """Recibe los datos del usuario, realiza las comprabaciones y añade el nuevo usuario a la base de datos"""
+        #Coge los valores
         nombre = self.nombre_entry.get()
         apellido = self.apellido_entry.get()
         fecha_nacimiento = self.fecha_nacimiento_entry.get()
         dni = self.dni_registro_entry.get()
         contrasena = self.contrasena_registro_entry.get()
+        #Hace comprobacione
         if nombre != "" and apellido != "" and self.comprobar_fecha() and self.comprobar_dni() and self.comprobar_contrasena():
+            #Asigna la contraseña de registro al atributo clave para poder encriptar los datos antes de almacenarlos
             self.clave=contrasena
+            #Cifra la clave
             con_cifrada, salt = self.cifrar_clave(self.clave)
-            # print(type(con_cifrada))
+
             con_cifrada_hex = con_cifrada.hex()
             salt_hex = salt.hex()
             # al registrar al usuario ciframos los datos de nombre, apellido y fecha de nacimiento usando el dni
-            # guarda el dato encriptado y seguidamente el nonce
+            # guarda el dato encriptado  y seguidamente el nonce
             nombre_encr, nonce_nombre = self.encriptar(nombre, salt)
             apellido_encr, nonce_apellido = self.encriptar(apellido, salt)
             fecha_encr, nonce_fecha = self.encriptar(fecha_nacimiento, salt)
+            #Lo guardamos en formato hexadecimal para que tenga una visibilidad mejor la base de datos
             n_usuario = [dni, con_cifrada_hex, salt_hex, nombre_encr.hex(), nonce_nombre.hex(), apellido_encr.hex(), nonce_apellido.hex(), fecha_encr.hex(), nonce_fecha.hex(),"",""]
             self.addto_csv(self.nombre_basedatos,n_usuario)
-            #print( nombre_encr, nonce_nombre,salt,salt_hex, nombre_encr.hex(), nonce_nombre.hex())
-
             self.ventana_registro.destroy()
 
 
